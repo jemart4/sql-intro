8591897c1308f49b0d00ca3a4c43fc06c0f63919
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-01-16 01:07:38 +0000

    lab 1-3

--- demo.sql
+++ demo.sql
@@ -1,6 +1,6 @@
 -- Configure mode and headers columns
 -- Note: only needs to be configured once
-.mode columns
+.mode columns 
 .headers on
 
 -- Get the first and last name of every player in the database.
