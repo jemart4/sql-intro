02f17fbee505833723f808e8dc4ceea777def0ad
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-01-20 22:03:03 +0000

    4-4

--- lab-4-4.sql
+++ lab-4-4.sql
@@ -36,5 +36,12 @@
 -- | Toronto Blue Jays             | Randal     | Grichuk     | 31                   |
 -- | Washington Nationals          | Anthony    | Rendon      | 34                   |
 -- +-------------------------------+------------+-------------+----------------------+
+.mode columns 
+.headers on
 
-
+SELECT name, players.first_name, players.last_name, MAX(stats.home_runs) FROM stats 
+INNER JOIN teams ON stats.team_id = teams.id
+INNER JOIN players ON stats.player_id = players.id
+WHERE teams.year=2019
+GROUP BY teams.name
+;
